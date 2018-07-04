4ec90e3a6924cf3bac5c64692495c2397bdc0789
SergeiTishkov <35380498+SergeiTishkov@users.noreply.github.com>
2018-07-04 21:30:39 +0300

    Interface HW

--- HW_2_3_Tishkov_Sergei/InterfaceHW/Program.cs
+++ HW_2_3_Tishkov_Sergei/InterfaceHW/Program.cs
@@ -24,24 +24,5 @@ namespace InterfaceHW
                 Console.Clear();
             }
         }
-
-        static void RandomTest()
-        {
-            Random rnd = new Random((int)DateTime.Now.Ticks);
-            Dictionary<bool, int> dict = new Dictionary<bool, int>
-            {
-                { true, 0 },
-                { false, 0 }
-            };
-            for (int i = 0; i < 1000000; i++)
-            {
-                //Console.WriteLine(rnd.NextDouble() > 0.5);
-                dict[rnd.NextDouble() > 0.5]++;
-            }
-            foreach (var kpv in dict)
-            {
-                Console.WriteLine($"{kpv.Key} {kpv.Value}");
-            }
-        }
     }
 }
